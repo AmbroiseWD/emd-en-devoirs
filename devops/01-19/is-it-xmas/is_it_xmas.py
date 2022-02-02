9eb60ec8f5b3904dd807e0be596bb2af125ac4de
AmbroiseWD <72383670+AmbroiseWD@users.noreply.github.com>
2022-02-02 19:40:26 +0100

    Update is_it_xmas.py

--- devops/01-19/is-it-xmas/is_it_xmas.py
+++ devops/01-19/is-it-xmas/is_it_xmas.py
@@ -6,5 +6,5 @@ def is_it_xmas():
     day = today.day
     month = today.month
     print(f"[code] I think that now is: {day}/{month}")
-    it_is_xmas = day == 24 and month == 12
+    it_is_xmas = day == 23 and month == 12
     return it_is_xmas
